c7819e81189f726fa02b9845cd71ac195e264adc
sleong110 <sleong110@gmail.com>
2018-03-09 15:03:45 +0800

    Initial commit of Sequential Search in Python
    Implementation of sequential search and ordered sequential search in Python

---
+++ sequentialSearch.py
@@ -0,0 +1,23 @@
+def sequentialSearch(alist, item):
+    pos = 0
+    found = False
+    while pos < len(alist) and not found:
+        if alist[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+    return found 
+    
+def orderedSequentialSearch(alist, item):
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        else:
+            if alist[pos] > item:
+                stop = True
+            else:
+                pos = post + 1
+    return found 
